54c113f0c3aad57cd2a201d483cce3051e40d7dc
Cristopher Del Pozo Catalán <cdelpozo@pentafinanciero.cl>
2023-03-14 18:30:57 -0300

    Modulo 2 terminado

--- laboratorios/modulo-2/ejercicio-2/routes/users.js
+++ laboratorios/modulo-2/ejercicio-2/routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    usuarios = [
+        { Username: 'cronaldo', Nombre: 'Cristiano Ronaldo' },
+        { Username: 'lmessi', Nombre: 'Cristiano Ronaldo' },
+        { Username: 'njunior', Nombre: 'Neymar Junior' }
+    ];
+    res.render('usuarios', { title: 'Usuarios del Sistema', usuarios: usuarios });
 });
 
 module.exports = router;
